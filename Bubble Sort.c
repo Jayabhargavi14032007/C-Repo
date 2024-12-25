4a907e34086a31c80e96f5d4f8ffbd1a5852391f
Jayabhargavi14032007 <24B11EC194@adityauniversity.in>
2024-12-25 16:15:47 +0530

    Uploading code: Bubble Sort.c

---
+++ Bubble Sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int sr=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+                sr++;
+            }
+        }
+    }
+    printf("Number of swaps: %d",sr);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
